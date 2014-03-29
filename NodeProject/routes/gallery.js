2b79d7cf69b8385a33030f78f8211bfdbeb26351
yndai <tony.y.dai@gmail.com>
2014-03-29 17:19:46 -0400

    img gallery display

--- NodeProject/routes/gallery.js
+++ NodeProject/routes/gallery.js
@@ -5,8 +5,36 @@
 var app = require('../app');
 var fs = require('fs');
 
+function getTimestamp(){
+	
+	var date = new Date();
+	
+	var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+	
+	return year+month+day+hour+min+sec;
+}
+
+function listImages(){
+	var arr = [];
+	fs.readdirSync("public/images").forEach( function(file){
+		arr.push("/images/" + file);
+	});
+	return arr;
+}
+
 app.get('/gallery', function(req, res, next){
-	var data = {title : 'Image Gallery'};
+	var data = {title : 'Image Gallery',
+				imageList : listImages()};
 	res.render('gallery', data);
 
 });
@@ -29,35 +57,19 @@ app.post('/gallery-upload', function(req, res, next){
 					if (err) throw err;
 				});
 			}
+			
 			var data = {title : 'Image Gallery',
-						msg : 'File uploaded'};
+						msg : 'File uploaded',
+						imageList : listImages()};
 			res.render('gallery', data);
 		});
 		
 		
 	} else {
 		var data = {title : 'Image Gallery',
-					msg : 'File missing or incorrect type'};
+					msg : 'File missing or incorrect type',
+					imageList : listImages()};
 		res.render('gallery', data);
 	}
 	
 });
-
-function getTimestamp(){
-	
-	var date = new Date();
-	
-	var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-	
-	return year+month+day+hour+min+sec;
-}
